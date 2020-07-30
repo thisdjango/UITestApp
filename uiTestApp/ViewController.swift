393192471911ea1d013be3e3ddf1db91a0c0f85b
Diana <tsarkova.92@mail.ru>
2020-07-30 16:15:30 +0300

    Initial Commit

---
+++ uiTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiTestApp
+//
+//  Created by Diana Tsarkova on 30.07.2020.
+//  Copyright © 2020 Diana Tsarkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
